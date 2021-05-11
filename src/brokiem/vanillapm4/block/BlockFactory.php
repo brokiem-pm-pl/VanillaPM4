44a36d3bb5cd61aefceab0eb40200ad6ab829b0a
brokiem <milyhqvideo@gmail.com>
2021-05-11 23:18:36 +0800

    add bell

--- src/brokiem/vanillapm4/block/BlockFactory.php
+++ src/brokiem/vanillapm4/block/BlockFactory.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace brokiem\vanillapm4\block;
 
 use brokiem\vanillapm4\tile\ShulkerBox as TileShulkerBox;
+use pocketmine\block\BlockBreakInfo;
 use pocketmine\block\BlockFactory as BlockFactoryPM;
 use pocketmine\block\BlockIdentifier as BID;
 use pocketmine\block\BlockLegacyIds as Ids;
+use pocketmine\block\BlockToolType;
+use pocketmine\block\Transparent;
 use pocketmine\item\ItemIds;
 
 class BlockFactory {
@@ -22,5 +25,6 @@ class BlockFactory {
         $i->register(new Lectern(new BID(Ids::LECTERN, 0), "Lectern"));
         $i->register(new Campfire(new BID(Ids::CAMPFIRE, 0, ItemIds::CAMPFIRE), "Campfire"));
         $i->register(new Composter(new BID(Ids::COMPOSTER, 0), "Composter"));
+        $i->register(new Transparent(new BID(Ids::BELL, 0), "Bell", new BlockBreakInfo(1, BlockToolType::PICKAXE, 0, 25)));
     }
 }
